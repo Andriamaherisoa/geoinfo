3c49fe57bdd5806534606de7345c680f38626ec4
th150233 <hippolyte.taquet@etu.univ-lehavre.fr>
2021-02-15 20:47:35 +0100

    sqlite

--- app/src/main/java/com/unlh/geoinfo/MainActivity.java
+++ app/src/main/java/com/unlh/geoinfo/MainActivity.java
@@ -23,6 +23,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        MySQLiteHelper sql = new MySQLiteHelper(this);
         setContentView(R.layout.activity_main);
     }
 

8dc0ee2c6ec8f9b25784e37633085729cec3e6c1
Mohamed Alpha KEITA <mohamed-alpha.keita@etu.univ-lehavre.fr>
2021-02-15 02:30:33 +0100

    Add google map API and map activity

---
+++ app/src/main/java/com/unlh/geoinfo/MapActivity.java
@@ -0,0 +1,29 @@
+package com.unlh.geoinfo;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentActivity;
+
+import android.os.Bundle;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+
+public class MapActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    GoogleMap map;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+
+    }
+}
